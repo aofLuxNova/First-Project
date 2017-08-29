afbfbd4e00905b136ec8739a7b87e4bb47c23d10
slfan2013 <fansili2013@gmail.com>
2017-08-29 09:25:06 -0700

    a

---
+++ data reading and formating.R
@@ -0,0 +1,24 @@
+#-------------------- data reading & formating --------------------#
+#--------------------        8/29/2017         --------------------#
+
+# 0. Pull before you start editing your code.
+# 1. Please add some comment.
+# 2. Google if you got stuck. 
+# 3. Use "?functionname"  in Console to learn detail information about a function. For example, ?data.frame tell you everything about function data.frame().
+# 4. Ask me anytime if google didn't answer your question. Don't use email. Use wechat.
+# 5. Push after you finish your editing.
+# some basic functions you may consider are, install.packages(), is.na(), which(), microbenchmark().
+
+
+
+# read .xlsx data (example.xlsx).
+
+# format it into three data.frames, eData, fData and pData. see examples.
+
+# save eData, fData and pData into a .csv files.
+
+# *try multiple ways of reading .xlsx data into R and find out which is the fastest way of doing so (checkout http://adv-r.had.co.nz/Performance.html Microbenchmarking).
+
+
+
+
