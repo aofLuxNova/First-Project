facdb2560d6fba276198fb9e743dc1c60c8562fb
aofLuxNova <joesupernovae@live.com>
2018-01-04 21:50:20 -0800

    Polish for report
    Added details to volcano plot, additional columns in results

--- write xlsx.R
+++ write xlsx.R
@@ -4,9 +4,9 @@ library(openxlsx)
 source("t_test_foldchange_reading.r")
 my_list <- readData(path = "E:\\Documents\\Programming\\R\\excel\\mx 368045 Jayoung Kim_human urine_09-2017_submit.xlsx")
 
-# write.csv(my_list$fData, file = "f.csv", append = FALSE, qmethod = D, quote = TRUE)
-# write.csv(my_list$pData, file = "p.csv", append = FALSE, qmethod = D, quote = TRUE)
-# write.csv(my_list$eData, file = "e.csv", append = FALSE, qmethod = D, quote = TRUE)
+# write.csv(my_list$eData, file = "e.csv", append = FALSE, qmethod = D, quote = TRUE, row.names = TRUE)
+# write.csv(my_list$fData, file = "f.csv", append = FALSE, qmethod = D, quote = TRUE, row.names = FALSE)
+# write.csv(my_list$pData, file = "p.csv", append = FALSE, qmethod = D, quote = TRUE, row.names = FALSE)
 
 e <- my_list$eData
 colnames(e) = my_list$pData$comment
@@ -25,6 +25,8 @@ sa.p = mapply(function(x,y){
 },x=sa,y=sb)
 
 list.p <- sa.p["p.value",]
+list.p.neglog = -log10(as.numeric(list.p))
+
 
 # fold change
 
@@ -36,8 +38,13 @@ sa.f = mapply(function(x,y){
   x/y
 },x=sa.avg,y=sb.avg)
 
-# result = cbind(list.p, sa.f); colnames(result) = c("p-value","fold change")
-# write.csv(result, file = "Kim_Result.csv", append = FALSE, qmethod = D, quote = TRUE)
+sa.f.log = log2(sa.f)
+
+#result = cbind(list.p, sa.f, list.p.neglog, sa.f.log)
+#colnames(result) = c("p-value","fold change","Negative log10(p-value)","log2(fold change)")
+#write.csv(result, file = "Kim_Result.csv", append = FALSE, qmethod = D, quote = TRUE)
 
 par(mar = c(5,5,2,1), mgp = c(3.5, 2, 1), tck = -0.02)
-plot(log2(sa.f), -log10(as.numeric(list.p)), xlim = c(-5,5), ylim = c(0,10), xlab = "Log2 Fold Change", ylab = "-log10 p-value")
+plot(sa.f.log, list.p.neglog, xlim = c(-4,4), ylim = c(0,10), 
+     main = "Male vs. Female \nUrine Metabolic Profile Differences", 
+     xlab = "Log2 Fold Change", ylab = "-log10 p-value")
